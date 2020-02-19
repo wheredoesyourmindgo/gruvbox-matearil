2146faba256de34c7a2b1d8b259015e5d3da3e90
pcwa-ahendricks <ahendricks@pcwa.net>
2020-02-19 14:12:21 -0800

    Only un-italicize when not parsing a comment.

--- src/syntax/default.ts
+++ src/syntax/default.ts
@@ -4,13 +4,15 @@ import { getItalicSyntax } from "./italic";
 export function getDefaultSyntax(palette: Palette, italicComments: boolean) {
   const syntax = getItalicSyntax(palette, italicComments);
   const noItalicSyntax = syntax.map(tmr => {
-    const { settings, ...rest } = tmr;
-    Object.keys(settings).forEach(k => {
-      if (k === "fontStyle" && settings[k] === "italic") {
-        settings[k] = "";
-      }
-    });
-    return { ...rest, settings };
+    const { name, settings, ...rest } = tmr;
+    if (!/^comment$/i.test(name)) {
+      Object.keys(settings).forEach(k => {
+        if (k === "fontStyle" && settings[k] === "italic") {
+          settings[k] = "";
+        }
+      });
+    }
+    return { ...rest, name, settings };
   });
   return noItalicSyntax;
 }
